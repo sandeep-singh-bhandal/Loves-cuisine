40ac0b4e6bc67655229c3bd9de9b56789e2a8f15
Sunny <155150793+sandeep-310@users.noreply.github.com>
2024-08-31 20:45:53 +0530

    bugfix:minor

--- app/loading.jsx
+++ app/loading.jsx
@@ -1,16 +1,6 @@
-import { useEffect } from 'react'
 import styles from './Loader.module.scss'
 
 function Loader() {
-  useEffect(() => {
-    window.onload = () =>
-      setInterval(() => {
-        document
-          .querySelector(`.${styles.loader}`)
-          .classList.add(styles['fade-out'])
-      }, 3000)
-  }, [])
-
   return (
     <div className={styles.loader}>
       <img src='/images/loader.gif' alt='loader' />
